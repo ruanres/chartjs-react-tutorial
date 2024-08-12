4c83e44cf4768632f87ae7059153907ebfd64e57
Ruan Eloy <ruanr18@gmail.com>
2024-08-11 21:27:12 -0300

    refactor: remove unused vars

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
+import "chart.js/auto";
 import BarChart from "./components/BarChart";
 import LineChart from "./components/LineChart";
 import PieChart from "./components/PieChart";
 import { UserData } from "./Data";
 
+const userData = {
+  labels: UserData.map((data) => data.year),
+  datasets: [
+    {
+      label: "Users Gained",
+      data: UserData.map((data) => data.userGain),
+      backgroundColor: [
+        "rgba(75,192,192,1)",
+        "#ecf0f1",
+        "#50AF95",
+        "#f3ba2f",
+        "#2a71d0",
+      ],
+      borderColor: "black",
+      borderWidth: 2,
+    },
+  ],
+};
+
 function App() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-  // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
-
   return (
     <div className="App">
       <h2>Charts</h2>
